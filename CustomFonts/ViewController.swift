12232083807050d49d24780ad15e30e56965ce22
mohamedosama2074 <iosengks74@gmail.com>
2019-09-29 20:35:52 +0200

    Initial Commit

---
+++ CustomFonts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomFonts
+//
+//  Created by Mohamed on 9/29/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
